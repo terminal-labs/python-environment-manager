d3ad83992a2385c559e7312f26f177c7573b48a6
Michael Verhulst <michael@terminallabs.com>
2019-07-06 19:04:18 -0500

    Update install_app.sh

--- maintenance/vagrant/pyenv/install_app.sh
+++ maintenance/vagrant/pyenv/install_app.sh
@@ -1,5 +1,10 @@
+export APPNAME=$1
+export USERNAME=$2
 su -m vagrant <<'EOF'
   source maintenance/vagrant/pyenv/env.sh
-  cd pyvirtualbox
-  pip install .
+  if [ -d $APPNAME ] 
+  then
+    eval cd $APPNAME
+    pip install --user .
+  fi
 EOF
