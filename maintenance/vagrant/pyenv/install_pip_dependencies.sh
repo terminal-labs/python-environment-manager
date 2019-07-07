4d848f13cf9121872012d6c2a7f232b2b423ad4c
Michael Verhulst <michael@terminallabs.com>
2019-07-06 22:18:50 -0500

    Update and rename install_pip_requirements.sh to install_pip_dependencies.sh

--- maintenance/vagrant/pyenv/install_pip_dependencies.sh
+++ maintenance/vagrant/pyenv/install_pip_dependencies.sh
@@ -2,8 +2,8 @@ export APPNAME=$1
 export USERNAME=$2
 su -m vagrant <<'EOF'
   source maintenance/vagrant/pyenv/env.sh
-  if [ -f requirements.txt ] 
+  if [ -f dependencies/pip.txt ] 
   then
-    pip install --user -r requirements.txt
+    pip install --user -r dependencies/pip.txt
   fi
 EOF
