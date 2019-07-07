fdbb3ec51c3f5508b6388f070a772aec518021fb
Michael Verhulst <michael@terminallabs.com>
2019-07-06 22:17:56 -0500

    Update and rename install_deps.sh to install_apt_dependencies.sh

--- maintenance/vagrant/pyenv/install_apt_dependencies.sh
+++ maintenance/vagrant/pyenv/install_apt_dependencies.sh
@@ -1,4 +1,3 @@
-# start of section that inherits sudo
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 echo "resolvconf resolvconf/linkify-resolvconf boolean false" | debconf-set-selections
 DEBIAN_FRONTEND=noninteractive apt -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" update
@@ -6,11 +5,10 @@ DEBIAN_FRONTEND=noninteractive apt -y -o DPkg::options::="--force-confdef" -o DP
 
 apt-get update -y --quiet
 apt-get upgrade -y --quiet
+apt-get install -y -f
 
 apt update -y --quiet
 apt upgrade -y --quiet
-
-apt-get install -y -f
 apt install -y -f
 
 apt install -y --quiet ca-certificates
@@ -18,21 +16,4 @@ apt install -y --quiet ca-certificates
 apt install -y build-essential
 apt install -y linux-headers-$(uname -r)
 
-apt install -y \
-    libssl-dev \
-    zlib1g-dev \
-    libbz2-dev \
-    libreadline-dev \
-    libsqlite3-dev \
-    libffi-dev \
-    zip \
-    unzip \
-
-apt install -y wget
-apt install -y nano
-apt install -y emacs
-
-apt install -y python3
-apt install -y python3-pip
-apt install -y python3-dev
-apt install -y python3-venv
+xargs apt install -y < dependencies/apt.txt
