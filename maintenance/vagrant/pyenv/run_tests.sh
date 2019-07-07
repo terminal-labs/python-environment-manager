5b33d267de0a9a94f638ecb27c79ab6d791b96e3
Michael Verhulst <michael@terminallabs.com>
2019-07-06 19:03:37 -0500

    Update run_tests.sh

--- maintenance/vagrant/pyenv/run_tests.sh
+++ maintenance/vagrant/pyenv/run_tests.sh
@@ -1,6 +1,11 @@
+export APPNAME=$1
+export USERNAME=$2
 su -m vagrant <<'EOF'
   source maintenance/vagrant/pyenv/env.sh
-  pyvirtualbox system version
-  pyvirtualbox system selftest
-  pyvirtualbox system selfcoverage
+  if [ -d $APPNAME ] 
+  then
+    eval $APPNAME system version
+    eval $APPNAME system selftest
+    eval $APPNAME system selfcoverage
+  fi
 EOF
