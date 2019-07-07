20105e4c42cca00bf2a6850bdc509474661445ca
Michael Verhulst <michael@terminallabs.com>
2019-07-06 22:16:59 -0500

    Update build.sh

--- maintenance/vagrant/pyenv/build.sh
+++ maintenance/vagrant/pyenv/build.sh
@@ -2,9 +2,9 @@ export APPNAME=$1
 export USERNAME=$2
 su -m $USERNAME <<'EOF'
   eval "vagrant up"
-  eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/vagrant/pyenv/install_deps.sh $APPNAME $USERNAME'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/vagrant/pyenv/install_apt_dependencies.sh $APPNAME $USERNAME'"
   eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/vagrant/pyenv/install_python.sh $APPNAME $USERNAME'"
-  eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/vagrant/pyenv/install_pip_requirements.sh $APPNAME $USERNAME'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/vagrant/pyenv/install_pip_dependencies.sh $APPNAME $USERNAME'"
   eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/vagrant/pyenv/install_app.sh $APPNAME $USERNAME'"
   eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/vagrant/pyenv/run_tests.sh $APPNAME $USERNAME'"
 EOF
