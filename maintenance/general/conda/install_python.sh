d3f3f02af9f72827e5a6ac5410920946519a27f7
Michael Verhulst <michael@terminallabs.com>
2019-07-08 15:35:28 -0500

    Update install_python.sh

--- maintenance/general/conda/install_python.sh
+++ maintenance/general/conda/install_python.sh
@@ -1,23 +1,23 @@
-export LC_APPNAME=$1
-export LC_USERNAME=vagrant
-export LC_PLATFORM=$3
-su -m ${LC_USERNAME} <<'EOF'
-  USER=${LC_USERNAME}
-  SUDO_USER=${LC_USERNAME}
-  USERNAME=${LC_USERNAME}
-  HOME=/home/${LC_USERNAME}
-  LOGNAME=/home/${LC_USERNAME}  
+export APPNAME=$1
+export USERNAME=vagrant
+export PLATFORM=$3
+su -m ${USERNAME} <<'EOF'
+  USER=${USERNAME}
+  SUDO_USER=${USERNAME}
+  USERNAME=${USERNAME}
+  HOME=/home/${USERNAME}
+  LOGNAME=/home/${USERNAME}  
   unset SUDO_UID SUDO_GID SUDO_USER
   printenv    
-  if [ $LC_PLATFORM == "vagrant" ]; then
+  if [ $PLATFORM == "vagrant" ]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/vagrant/miniconda3
     export PATH="/home/vagrant/miniconda3/bin:$PATH"
-  elif [ $LC_PLATFORM == "mac" ]
+  elif [ $PLATFORM == "mac" ]
   then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
     bash Miniconda3-latest-MacOSX-x86_64.sh -b
-    export PATH="/Users/${LC_USERNAME}/miniconda3/bin:$PATH"
+    export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
   else
     echo "not implemented yet"
   fi
@@ -25,8 +25,8 @@ su -m ${LC_USERNAME} <<'EOF'
   conda --version
   conda update -y conda
   conda --version
-  conda create -y -n ${LC_APPNAME} python=3.6
-  source activate ${LC_APPNAME}
+  conda create -y -n ${APPNAME} python=3.6
+  source activate ${APPNAME}
 
   pip install --upgrade setuptools
   pip install --upgrade pip
