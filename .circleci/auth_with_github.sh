04e5673c326725d3d4269a6a6c2aeb6e0572bf5e
Michael Verhulst <michael@terminallabs.com>
2019-07-08 13:10:22 -0500

    Update auth_with_github.sh

--- .circleci/auth_with_github.sh
+++ .circleci/auth_with_github.sh
@@ -7,6 +7,7 @@ ssh-keygen -t rsa -N "" -f /home/"$USER"/.ssh/id_rsa
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" > /home/"$USER"/.ssh/config
 ssh-keyscan github.com >> /home/"$USER"/.ssh/known_hosts
 
+# circleci
 sudo touch /home/circleci/.ssh/authorized_keys
 
 sudo chown -R "$USER" /home/"$USER"/.ssh
@@ -18,7 +19,22 @@ sudo chmod 644 /home/"$USER"/.ssh/config
 sudo chmod 600 /home/"$USER"/.ssh/id_rsa
 sudo chmod 644 /home/"$USER"/.ssh/id_rsa.pub
 
-sudo cp -a /home/"$USER"/.ssh/. /root/.ssh/
+# vagrant
+sudo cp -a /home/"$USER"/.ssh/. /home/vagrant/.ssh/
+
+sudo touch /home/vagrant/.ssh/authorized_keys
+
+sudo chown -R vagrant /home/vagrant/.ssh
+sudo chgrp -R vagrant /home/vagrant/.ssh
+sudo chmod 400 /home/vagrant/.ssh
+sudo chmod 400 /home/vagrant/.ssh/authorized_keys
+sudo chmod 400 /home/vagrant/.ssh/known_hosts
+sudo chmod 400 /home/vagrant/.ssh/config
+sudo chmod 400 /home/vagrant/.ssh/id_rsa
+sudo chmod 400 /home/vagrant/.ssh/id_rsa.pub
+
+# root
+sudo cp -a /home/"$USER"/.ssh/. /home/vagrant/.ssh/
 
 sudo touch /root/.ssh/authorized_keys
 
