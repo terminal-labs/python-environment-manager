42413e9d8578591ac959e8d048829889ab2d8689
Michael Verhulst <michael@terminallabs.com>
2019-07-11 02:02:15 -0500

    Create uuid-accounts.sh

---
+++ incidental-mocking/uuid-accounts.sh
@@ -0,0 +1,21 @@
+cd ~
+
+mkdir inflation_system
+mkdir inflation_system/accounts
+
+mkdir inflation_system/accounts/893afdde-b90b-4f88-86ab-a1a1b43a5a9d
+mkdir inflation_system/accounts/893afdde-b90b-4f88-86ab-a1a1b43a5a9d/clusters
+mkdir inflation_system/accounts/893afdde-b90b-4f88-86ab-a1a1b43a5a9d/clusters/58a51d9b-49fb-4275-8be1-bcd6d5970a6f
+mkdir inflation_system/accounts/893afdde-b90b-4f88-86ab-a1a1b43a5a9d/clusters/58a51d9b-49fb-4275-8be1-bcd6d5970a6f/logs
+touch inflation_system/accounts/893afdde-b90b-4f88-86ab-a1a1b43a5a9d/clusters/58a51d9b-49fb-4275-8be1-bcd6d5970a6f/name
+touch inflation_system/accounts/893afdde-b90b-4f88-86ab-a1a1b43a5a9d/clusters/58a51d9b-49fb-4275-8be1-bcd6d5970a6f/size
+touch inflation_system/accounts/893afdde-b90b-4f88-86ab-a1a1b43a5a9d/clusters/58a51d9b-49fb-4275-8be1-bcd6d5970a6f/type
+
+
+mkdir inflation_system/accounts/9dd23851-6787-4dc3-b261-088d0c74b55c
+mkdir inflation_system/accounts/9dd23851-6787-4dc3-b261-088d0c74b55c/clusters
+mkdir inflation_system/accounts/9dd23851-6787-4dc3-b261-088d0c74b55c/clusters/e5fc3ef0-bdf6-4328-9e1b-99d77490fb6b
+mkdir inflation_system/accounts/9dd23851-6787-4dc3-b261-088d0c74b55c/clusters/e5fc3ef0-bdf6-4328-9e1b-99d77490fb6b/logs
+touch inflation_system/accounts/9dd23851-6787-4dc3-b261-088d0c74b55c/clusters/e5fc3ef0-bdf6-4328-9e1b-99d77490fb6b/name
+touch inflation_system/accounts/9dd23851-6787-4dc3-b261-088d0c74b55c/clusters/e5fc3ef0-bdf6-4328-9e1b-99d77490fb6b/size
+touch inflation_system/accounts/9dd23851-6787-4dc3-b261-088d0c74b55c/clusters/e5fc3ef0-bdf6-4328-9e1b-99d77490fb6b/type
