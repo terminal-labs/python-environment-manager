afad468f97b2aa28963475e180df163dddd406ff
Michael Verhulst <michael@terminallabs.com>
2019-07-11 22:57:32 -0500

    Update env.sh

--- maintenance/general/pyenv/env.sh
+++ maintenance/general/pyenv/env.sh
@@ -1,19 +1,24 @@
-  export PY_ENV_VERSION=3.6.6
+export PY_ENV_VERSION=3.6.6
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+if [ $PLATFORM == "vagrant" ]; then
+  export PYENV_ROOT="/home/vagrant/pyenv/envs/${APPNAME}/.pyenv"
   export PATH="${PYENV_ROOT}/bin:${PATH}"
-  if [ $PLATFORM == "vagrant" ]; then
-    export PYENV_ROOT="/home/vagrant/pyenv/envs/${APPNAME}/.pyenv"
-    export PATH="${PYENV_ROOT}/bin:${PATH}"
-    export PATH="/home/vagrant/.local/bin:${PATH}"
-  elif [ $PLATFORM == "mac" ]
-  then
-    export PYENV_ROOT=/Users/${USERNAME}/pyenv/envs/${APPNAME}/.pyenv
-    export PATH="${PYENV_ROOT}/bin:${PATH}"
-    export PATH="/Users/${USERNAME}/.local/bin:${PATH}"
-  else
-    echo "not implemented yet"
-  fi
-  export USE_GIT_URI="true"
-  eval "$(pyenv init -)"
-  pyenv install -s ${PY_ENV_VERSION}
-  pyenv global ${PY_ENV_VERSION}
-  pyenv global
+  export PATH="/home/vagrant/.local/bin:${PATH}"
+elif [ $PLATFORM == "linux" ]
+then
+  export PYENV_ROOT=/home/${USERNAME}/pyenv/envs/${APPNAME}/.pyenv
+  export PATH="${PYENV_ROOT}/bin:${PATH}"
+  export PATH="/home/${USERNAME}/.local/bin:${PATH}"
+elif [ $PLATFORM == "mac" ]
+then
+  export PYENV_ROOT=/Users/${USERNAME}/pyenv/envs/${APPNAME}/.pyenv
+  export PATH="${PYENV_ROOT}/bin:${PATH}"
+  export PATH="/Users/${USERNAME}/.local/bin:${PATH}"
+else
+  echo "not implemented yet"
+fi
+export USE_GIT_URI="true"
+eval "$(pyenv init -)"
+pyenv install -s ${PY_ENV_VERSION}
+pyenv global ${PY_ENV_VERSION}
+pyenv global
