15a7eff532589616209e838dc93a2519eb8be49f
Michael Verhulst <michael@terminallabs.com>
2019-07-14 01:01:37 -0500

    Create emit_activation_script.sh

---
+++ maintenance/general/pyenv/emit_activation_script.sh
@@ -0,0 +1,12 @@
+FILE="activate.sh"
+
+cat <<EOM >$FILE
+export APPNAME=utilitiespackage
+export USERNAME=user
+export PLATFROM=linux
+export PYENV_ROOT=/home/${USERNAME}/pyenv/envs/${APPNAME}/.pyenv
+export PATH="/Users/${USERNAME}/.local/bin:${PATH}"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+export USE_GIT_URI="true"
+eval "$(pyenv init -)"
+EOM
