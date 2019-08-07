9f5382546dc577580ca9d6529cd19cf953e3b0a7
Michael Verhulst <michael@terminallabs.com>
2019-08-07 00:08:53 -0500

    Create mac_activate.sh

---
+++ maintenance/general/pyenv/mac_activate.sh
@@ -0,0 +1,8 @@
+export APPNAME=utilitiespackage
+export USERNAME=mike
+export PLATFROM=mac
+export PYENV_ROOT=/Users/$USERNAME/pyenv/envs/$APPNAME/.pyenv
+export PATH="/Users/$USERNAME/.local/bin:$PATH"
+export PATH="$PYENV_ROOT/bin:$PATH"
+export USE_GIT_URI="true"
+eval "export PATH="/Users/mike/.pyenv/shims:${PATH}"
