5f6e660f8b3d4fc9bf3fcc642a1178450e9a98d6
Michael Verhulst <michael@terminallabs.com>
2019-08-07 00:07:25 -0500

    Update emit_activation_script.sh

--- maintenance/general/pyenv/emit_activation_script.sh
+++ maintenance/general/pyenv/emit_activation_script.sh
@@ -1,29 +1,13 @@
 export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
-su -m $USERNAME << EOF
-if [ $PLATFORM == "linux" ]; then
-  echo '
-  export APPNAME=$APPNAME
-  export USERNAME=$USERNAME
-  export PLATFROM=$PLATFORM
-  export PYENV_ROOT=/home/\$USERNAME/pyenv/envs/\$APPNAME/.pyenv
-  export PATH="/home/\$USERNAME/.local/bin:\$PATH"
-  export PATH="\$PYENV_ROOT/bin:\$PATH"
-  export USE_GIT_URI="true"
-  eval "$(pyenv init -)"' > activate.sh
-elif [ $PLATFORM == "mac" ]
-then
-  echo '
-  export APPNAME=$APPNAME
-  export USERNAME=$USERNAME
-  export PLATFROM=$PLATFORM
-  export PYENV_ROOT=/Users/\$USERNAME/pyenv/envs/\$APPNAME/.pyenv
-  export PATH="/Users/\$USERNAME/.local/bin:\$PATH"
-  export PATH="\$PYENV_ROOT/bin:\$PATH"
-  export USE_GIT_URI="true"
-  eval "$(pyenv init -)"' > activate.sh
-else
-  echo "not implemented yet"
-fi
+su -m $USERNAME << 'EOF'
+  if [ $PLATFORM == "linux" ]; then
+   cp maintenance/general/pyenv/linux_activate.sh activate.sh
+  elif [ $PLATFORM == "mac" ]
+  then
+    cp maintenance/general/pyenv/mac_activate.sh activate.sh
+  else
+    echo "not implemented yet"
+  fi
 EOF
