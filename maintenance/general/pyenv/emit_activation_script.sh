6d8d88c28648841de1ab05c680ca6868157462f7
Michael Verhulst <michael@terminallabs.com>
2019-08-06 23:40:43 -0500

    Update emit_activation_script.sh

--- maintenance/general/pyenv/emit_activation_script.sh
+++ maintenance/general/pyenv/emit_activation_script.sh
@@ -1,13 +1,29 @@
-export USERNAME=user
-export APPNAME=utilitiespackage
+export APPNAME=$1
+export USERNAME=$2
+export PLATFORM=$3
 su -m $USERNAME <<'EOF'
-echo '
-export APPNAME=utilitiespackage
-export USERNAME=user
-export PLATFROM=linux
-export PYENV_ROOT=/home/${USERNAME}/pyenv/envs/${APPNAME}/.pyenv
-export PATH="/home/${USERNAME}/.local/bin:${PATH}"
-export PATH="${PYENV_ROOT}/bin:${PATH}"
-export USE_GIT_URI="true"
-eval "$(pyenv init -)"' > activate.sh
+if [ $PLATFORM == "linux" ]; then
+  echo '
+  export APPNAME=utilitiespackage
+  export USERNAME=user
+  export PLATFROM=linux
+  export PYENV_ROOT=/home/${USERNAME}/pyenv/envs/${APPNAME}/.pyenv
+  export PATH="/home/${USERNAME}/.local/bin:${PATH}"
+  export PATH="${PYENV_ROOT}/bin:${PATH}"
+  export USE_GIT_URI="true"
+  eval "$(pyenv init -)"' > activate.sh
+elif [ $PLATFORM == "mac" ]
+then
+  echo '
+  export APPNAME=utilitiespackage
+  export USERNAME=user
+  export PLATFROM=linux
+  export PYENV_ROOT=/Users/${USERNAME}/pyenv/envs/${APPNAME}/.pyenv
+  export PATH="/Users$/{USERNAME}/.local/bin:${PATH}"
+  export PATH="${PYENV_ROOT}/bin:${PATH}"
+  export USE_GIT_URI="true"
+  eval "$(pyenv init -)"' > activate.sh
+else
+  echo "not implemented yet"
+fi
 EOF
