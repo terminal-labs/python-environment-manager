49ff4631099eb7d2bffe6d69e896683074c63fcb
Michael Verhulst <michael@terminallabs.com>
2019-08-07 00:10:50 -0500

    Update mac_activate.sh

--- maintenance/general/pyenv/mac_activate.sh
+++ maintenance/general/pyenv/mac_activate.sh
@@ -5,4 +5,3 @@ export PYENV_ROOT=/Users/$USERNAME/pyenv/envs/$APPNAME/.pyenv
 export PATH="/Users/$USERNAME/.local/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export USE_GIT_URI="true"
-eval "export PATH="/Users/mike/.pyenv/shims:${PATH}"
