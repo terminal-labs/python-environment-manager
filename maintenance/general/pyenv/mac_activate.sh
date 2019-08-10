15579afa2d0aa8ac2b4aba1aee8f95a8100447c0
Michael Verhulst <michael@terminallabs.com>
2019-08-10 13:06:54 -0500

    Update mac_activate.sh

--- maintenance/general/pyenv/mac_activate.sh
+++ maintenance/general/pyenv/mac_activate.sh
@@ -1,6 +1,6 @@
 export APPNAME=-appname-
 export USERNAME=-username-
-export PLATFROM=-platform-
+export PLATFORM=-platform-
 export PYENV_ROOT=/Users/$USERNAME/pyenv/envs/$APPNAME/.pyenv
 export PATH="/Users/$USERNAME/.local/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
