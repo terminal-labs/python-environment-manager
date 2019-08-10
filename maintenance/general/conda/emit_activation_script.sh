8f72663eb81021f7e01f560ea51aab0b3c164a32
Michael Verhulst <michael@terminallabs.com>
2019-08-10 13:36:17 -0500

    Update emit_activation_script.sh

--- maintenance/general/conda/emit_activation_script.sh
+++ maintenance/general/conda/emit_activation_script.sh
@@ -3,10 +3,10 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME << 'EOF'
   if [ $PLATFORM == "linux" ]; then
-   cp maintenance/general/pyenv/linux_activate.sh activate.sh
+   cp maintenance/general/conda/linux_activate.sh activate.sh
   elif [ $PLATFORM == "mac" ]
   then
-    cp maintenance/general/pyenv/mac_activate.sh activate.sh
+    cp maintenance/general/conda/mac_activate.sh activate.sh
     sed "s/-appname-/$APPNAME/g" activate.sh > activate.tmp
     rm activate.sh
     mv activate.tmp activate.sh
