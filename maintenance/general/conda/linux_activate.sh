238af94baba3d8d706fa7bc2bc81ad62c0e5caef
Michael Verhulst <michael@terminallabs.com>
2019-08-10 23:15:34 -0500

    Create linux_activate.sh

---
+++ maintenance/general/conda/linux_activate.sh
@@ -0,0 +1,6 @@
+export APPNAME=-appname-
+export USERNAME=-username-
+export PLATFORM=-platform-
+export PATH="/home/${USERNAME}/miniconda3/bin:$PATH"
+export PATH="/home/$USERNAME/.local/bin:$PATH"
+export USE_GIT_URI="true"
