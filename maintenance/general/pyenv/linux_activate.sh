bceb4baef1a6b75c9cfa546c9ab7f498b51ddb52
Michael Verhulst <michael@terminallabs.com>
2019-08-11 00:17:23 -0500

    Create linux_activate.sh

---
+++ maintenance/general/pyenv/linux_activate.sh
@@ -0,0 +1,7 @@
+export APPNAME=-appname-
+export USERNAME=-username-
+export PLATFORM=-platform-
+export PYENV_ROOT=/home/$USERNAME/pyenv/envs/$APPNAME/.pyenv
+export PATH="/home/$USERNAME/.local/bin:$PATH"
+export PATH="$PYENV_ROOT/bin:$PATH"
+export USE_GIT_URI="true"
