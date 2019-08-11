d0703cef57bff300438e2697191939ef85f30153
Michael Verhulst <michael@terminallabs.com>
2019-08-10 23:27:22 -0500

    Update emit_activation_script.sh

--- maintenance/general/conda/emit_activation_script.sh
+++ maintenance/general/conda/emit_activation_script.sh
@@ -3,20 +3,20 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME << 'EOF'
   if [ $PLATFORM == "linux" ]; then
-   cp maintenance/general/conda/linux_activate.sh activate.sh
+    cp maintenance/general/conda/linux_activate.sh activate.sh
   elif [ $PLATFORM == "mac" ]
   then
     cp maintenance/general/conda/mac_activate.sh activate.sh
-    sed "s/-appname-/$APPNAME/g" activate.sh > activate.tmp
-    rm activate.sh
-    mv activate.tmp activate.sh
-    sed "s/-username-/$USERNAME/g" activate.sh > activate.tmp
-    rm activate.sh
-    mv activate.tmp activate.sh  
-    sed "s/-platform-/$PLATFORM/g" activate.sh > activate.tmp
-    rm activate.sh
-    mv activate.tmp activate.sh
    else
     echo "not implemented yet"
   fi
+  sed "s/-appname-/$APPNAME/g" activate.sh > activate.tmp
+  rm activate.sh
+  mv activate.tmp activate.sh
+  sed "s/-username-/$USERNAME/g" activate.sh > activate.tmp
+  rm activate.sh
+  mv activate.tmp activate.sh  
+  sed "s/-platform-/$PLATFORM/g" activate.sh > activate.tmp
+  rm activate.sh
+  mv activate.tmp activate.sh
 EOF
