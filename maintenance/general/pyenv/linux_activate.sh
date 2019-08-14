7acfa093e99ce7a535d753238daa118d64311f15
Michael Verhulst <michael@terminallabs.com>
2019-08-14 00:54:59 -0500

    Update linux_activate.sh

--- maintenance/general/pyenv/linux_activate.sh
+++ maintenance/general/pyenv/linux_activate.sh
@@ -5,3 +5,8 @@ export PYENV_ROOT=/home/$USERNAME/pyenv/envs/$APPNAME/.pyenv
 export PATH="/home/$USERNAME/.local/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export USE_GIT_URI="true"
+export PY_ENV_VERSION=3.6.6
+eval "$(pyenv init -)"
+pyenv install -s ${PY_ENV_VERSION}
+pyenv global ${PY_ENV_VERSION}
+pyenv global
