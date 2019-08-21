bc8978eba6ae1846723e4280f52d36f3a59cb999
Michael Verhulst <michael@terminallabs.com>
2019-08-21 13:34:55 -0500

    Update install_python.sh

--- maintenance/general/pyenv/install_python.sh
+++ maintenance/general/pyenv/install_python.sh
@@ -22,13 +22,15 @@ su -m $USERNAME <<'EOF'
   else
     echo "not implemented yet"
   fi
+  export CFLAGS="-I$(brew --prefix openssl)/include" \
+  export LDFLAGS="-L$(brew --prefix openssl)/lib" \
   export PY_ENV_VERSION=3.6.9
   export SERVICE_USER=${USERNAME}
   export PATH="${PYENV_ROOT}/bin:${PATH}"
   export USE_GIT_URI="true"
   curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | /bin/bash
   eval "$(pyenv init -)"
-  pyenv install -s ${PY_ENV_VERSION}
+  pyenv install -s ${PY_ENV_VERSION} --verbose
   pyenv global ${PY_ENV_VERSION}
   pyenv global
   pip install --upgrade setuptools
