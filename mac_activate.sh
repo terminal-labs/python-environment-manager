57f9a45f095e84a8016e0020f95d5a43b03fb57b
Michael Verhulst <michael@terminallabs.com>
2019-08-21 13:06:18 -0500

    Delete mac_activate.sh

--- mac_activate.sh
+++
@@ -1,8 +0,0 @@
-export APPNAME=$APPNAME
-export USERNAME=$USERNAME
-export PLATFROM=$PLATFORM
-export PYENV_ROOT=/Users/$USERNAME/pyenv/envs/$APPNAME/.pyenv
-export PATH="/Users/$USERNAME/.local/bin:$PATH"
-export PATH="$PYENV_ROOT/bin:$PATH"
-export USE_GIT_URI="true"
-eval "$(pyenv init -)"' > activate.sh
