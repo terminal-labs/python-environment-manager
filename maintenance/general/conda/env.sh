2df53aee9c39876345148c09266e165fb286e392
Michael Verhulst <michael@terminallabs.com>
2019-08-21 11:29:53 -0500

    Update env.sh

--- maintenance/general/conda/env.sh
+++ maintenance/general/conda/env.sh
@@ -1,12 +1,12 @@
-if [ $PLATFORM == "vagrant" ]; then
+if [ $PLATFORM == "vagrant" ]
+then
   export PATH="/home/vagrant/miniconda3/bin:$PATH"
   export PATH="/home/vagrant/.local/bin:$PATH"
 elif [ $PLATFORM == "linux" ]
+then  
   export PATH="/home/${USERNAME}/miniconda3/bin:$PATH"
   export PATH="/home/${USERNAME}/.local/bin:$PATH"
   export PATH="/home/circleci/.local/bin:$PATH"
-then
-  export PATH="/home/${USERNAME}/miniconda3/bin:$PATH"
 elif [ $PLATFORM == "mac" ]
 then
   export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
