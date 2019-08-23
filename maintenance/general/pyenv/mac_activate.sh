5534a9120f3c643e0ea02b0553dbfaad13ec9d4c
Michael Verhulst <michael@terminallabs.com>
2019-08-23 15:18:23 -0500

    Update mac_activate.sh

--- maintenance/general/pyenv/mac_activate.sh
+++ maintenance/general/pyenv/mac_activate.sh
@@ -5,3 +5,8 @@ export PYENV_ROOT=/Users/$USERNAME/pyenv/envs/$APPNAME/.pyenv
 export PATH="/Users/$USERNAME/.local/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export USE_GIT_URI="true"
+export PY_ENV_VERSION=3.6.9
+eval "$(pyenv init -)"
+pyenv install -s ${PY_ENV_VERSION}
+pyenv global ${PY_ENV_VERSION}
+pyenv global
