912b335d6e9161cdd4cb25861c8fa773ef6f69b6
Michael Verhulst <michael@terminallabs.com>
2019-10-21 14:07:56 -0500

    Update create.sh

--- dpe/create.sh
+++ dpe/create.sh
@@ -72,8 +72,8 @@ su -m ${USERNAME} <<'EOF'
   git clone https://github.com/nvm-sh/nvm.git
   git clone https://github.com/lastpass/lastpass-cli.git
   git clone https://github.com/terminal-labs/utilities-package.git --recursive 
-  git clone git@github.com:terminal-labs/utilities-package_ml-metapackage.git --recursive
-  git clone git@github.com:terminal-labs/utilities-package_cli-metapackage.git --recursive
+  git clone https://github.com/terminal-labs/utilities-package_ml-metapackage.git --recursive
+  git clone https://github.com/terminal-labs/utilities-package_cli-metapackage.git --recursive
   git clone https://github.com/terminal-labs/rambo.git --recursive
   git clone https://github.com/terminal-labs/inflation.git --recursiv    
   cd ..
