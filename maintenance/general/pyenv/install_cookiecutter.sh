707d2ccd96e3b41588d594af7fab32189c1cc309
Michael Verhulst <michael@terminallabs.com>
2019-10-23 14:24:30 -0500

    Update install_cookiecutter.sh

--- maintenance/general/pyenv/install_cookiecutter.sh
+++ maintenance/general/pyenv/install_cookiecutter.sh
@@ -3,10 +3,7 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   source maintenance/general/pyenv/env.sh
-  if [ -d $APPNAME ] && [ ! -z $APPNAME ];
-  then
-    cd $APPNAME
-    pip install cookiecutter
-    cookiecutter /vagrant --no-input
-  fi
+  cd $APPNAME
+  pip install cookiecutter
+  cookiecutter /vagrant --no-input
 EOF
