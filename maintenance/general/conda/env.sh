3094daf6593e78a45a8c0365ee9b0e5de0a712ed
Michael Verhulst <michael@terminallabs.com>
2019-10-24 15:40:20 -0500

    Update env.sh

--- maintenance/general/conda/env.sh
+++ maintenance/general/conda/env.sh
@@ -3,18 +3,21 @@ then
   source /home/${USERNAME}/.bashrc
   export PATH="/home/vagrant/miniconda3/bin:$PATH"
   export PATH="/home/vagrant/.local/bin:$PATH"
+  HOME=/home/vagrant
 elif [ $PLATFORM == "linux" ]
 then
   source /home/${USERNAME}/.bashrc
   export PATH="/home/${USERNAME}/miniconda3/bin:$PATH"
   export PATH="/home/${USERNAME}/.local/bin:$PATH"
   export PATH="/home/circleci/.local/bin:$PATH"
+  HOME=/home/${USERNAME}
 elif [ $PLATFORM == "mac" ]
 then
   source /Users/${USERNAME}/.bash_profile
   export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
+  HOME=/Users/${USERNAME}
 else
   echo "not implemented yet"
 fi
-HOME=/home/vagrant
+
 conda activate ${APPNAME}
