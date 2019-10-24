55e82f0a62dc8278ddf09245ad870aff04ca220a
Michael Verhulst <michael@terminallabs.com>
2019-10-24 13:01:07 -0500

    Update install_cookiecutter.sh

--- maintenance/general/conda/install_cookiecutter.sh
+++ maintenance/general/conda/install_cookiecutter.sh
@@ -2,7 +2,8 @@ export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
-  source maintenance/general/conda/env.sh   
+  source maintenance/general/conda/env.sh
+  source maintenance/general/conda/env.sh  
   pip install cookiecutter
   cookiecutter /vagrant --no-input
 EOF
