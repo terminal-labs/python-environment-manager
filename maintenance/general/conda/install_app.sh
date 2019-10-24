acb66acd170f706fa5bb8a1b04d92372dfa8031e
Michael Verhulst <michael@terminallabs.com>
2019-10-24 16:09:03 -0500

    Update install_app.sh

--- maintenance/general/conda/install_app.sh
+++ maintenance/general/conda/install_app.sh
@@ -4,6 +4,7 @@ export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   source maintenance/general/conda/env.sh
   source /home/vagrant/.bashrc
+  HOME=/home/vagrant
   conda activate ${APPNAME}
   if [ -d $APPNAME ] && [ ! -z $APPNAME ];
   then
