a619bd2fd31b7d75ba72512aafa02403d7113db2
Michael Verhulst <michael@terminallabs.com>
2019-10-24 16:31:32 -0500

    Update install_app.sh

--- maintenance/general/conda/install_app.sh
+++ maintenance/general/conda/install_app.sh
@@ -6,9 +6,25 @@ su -m ${USERNAME} <<'EOF'
   SUDO_USER=${USERNAME}
   USERNAME=${USERNAME}
   unset SUDO_UID SUDO_GID SUDO_USER
+  if [ $PLATFORM == "vagrant" ]; then
+    HOME=/home/${USERNAME}
+    LOGNAME=/home/${USERNAME}
+    export PATH="/home/vagrant/miniconda3/bin:$PATH"
+  elif [ $PLATFORM == "linux" ]
+  then
+    HOME=/home/${USERNAME}
+    LOGNAME=/home/${USERNAME}
+    export PATH="/home/${USERNAME}/miniconda3/bin:$PATH"
+  elif [ $PLATFORM == "mac" ]
+  then
+    HOME=/Users/${USERNAME}
+    export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
+  else
+    echo "not implemented yet"
+  fi
+  conda init bash
   source maintenance/general/conda/env.sh
-  source /home/vagrant/.bashrc
-  HOME=/home/vagrant
+  source /home/${USERNAME}/.bashrc
   conda activate ${APPNAME}
   if [ -d $APPNAME ] && [ ! -z $APPNAME ];
   then
