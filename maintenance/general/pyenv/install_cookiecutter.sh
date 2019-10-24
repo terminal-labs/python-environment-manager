30406520e39ddc6efb71ce4fc49433780382db1c
Michael Verhulst <michael@terminallabs.com>
2019-10-23 22:53:53 -0500

    Update install_cookiecutter.sh

--- maintenance/general/pyenv/install_cookiecutter.sh
+++ maintenance/general/pyenv/install_cookiecutter.sh
@@ -3,6 +3,10 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   source maintenance/general/pyenv/env.sh
+  chown -R vagrant /vagrant
+  chmod -R 777 /vagrant
+  export USER=vagrant
+  export USERNAME=vagrant   
   pip install cookiecutter
   cookiecutter /vagrant --no-input
 EOF
