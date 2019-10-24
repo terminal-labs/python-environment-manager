7ea6409b1441db9cfed1ee0a589afc2e1544123a
Michael Verhulst <michael@terminallabs.com>
2019-10-24 16:36:50 -0500

    Update install_app.sh

--- maintenance/general/conda/install_app.sh
+++ maintenance/general/conda/install_app.sh
@@ -22,7 +22,6 @@ su -m ${USERNAME} <<'EOF'
   else
     echo "not implemented yet"
   fi
-  source maintenance/general/conda/env.sh
   source /home/${USERNAME}/.bashrc
   conda activate ${APPNAME}
   if [ -d $APPNAME ] && [ ! -z $APPNAME ];
