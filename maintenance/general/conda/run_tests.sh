3c4222266c2b188e7702965f2c49a8a9596237c8
Michael Verhulst <michael@terminallabs.com>
2019-10-24 13:19:32 -0500

    Update run_tests.sh

--- maintenance/general/conda/run_tests.sh
+++ maintenance/general/conda/run_tests.sh
@@ -2,8 +2,12 @@ export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
-  source maintenance/general/conda/env.sh
-  if [ -d $APPNAME ] && [ ! -z $APPNAME ];
+  USER=${USERNAME}
+  SUDO_USER=${USERNAME}
+  USERNAME=${USERNAME}
+  HOME=/home/vagrant
+  unset SUDO_UID SUDO_GID SUDO_USER   
+  source maintenance/general/conda/env.sh  if [ -d $APPNAME ] && [ ! -z $APPNAME ];
   then
     ${APPNAME} system version
     ${APPNAME} system selftest
