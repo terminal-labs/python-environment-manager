9d71b2161fa905eaccbade68cd12e5e9f17d84dd
Michael Verhulst <michael@terminallabs.com>
2019-10-24 15:07:37 -0500

    Update install_app.sh

--- maintenance/general/conda/install_app.sh
+++ maintenance/general/conda/install_app.sh
@@ -2,10 +2,6 @@ export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
-  USER=${USERNAME}
-  SUDO_USER=${USERNAME}
-  USERNAME=${USERNAME}
-  unset SUDO_UID SUDO_GID SUDO_USER
   source maintenance/general/conda/env.sh
   if [ -d $APPNAME ] && [ ! -z $APPNAME ];
   then
