8f2b8532f95cb59f53914917baaf103d68f655f8
Michael Verhulst <michael@terminallabs.com>
2019-10-23 22:53:14 -0500

    Update install_cookiecutter_dpe.sh

--- maintenance/general/pyenv/install_cookiecutter_dpe.sh
+++ maintenance/general/pyenv/install_cookiecutter_dpe.sh
@@ -3,7 +3,9 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   chown -R vagrant /vagrant
-  chmod -R 777 /vagrant  
+  chmod -R 777 /vagrant
+  export USER=vagrant
+  export USERNAME=vagrant  
   cd /vagrant/cookiecutterapp
   make createdpe
 EOF
