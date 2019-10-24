1bfbb7918f34ce33069a0501e3b57f831b97cb55
Michael Verhulst <michael@terminallabs.com>
2019-10-24 13:48:50 -0500

    Update install_app.sh

--- maintenance/general/conda/install_app.sh
+++ maintenance/general/conda/install_app.sh
@@ -2,7 +2,6 @@ export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
-  source /Users/${USERNAME}/.bash_profile
   source maintenance/general/conda/env.sh
   if [ -d $APPNAME ] && [ ! -z $APPNAME ];
   then
