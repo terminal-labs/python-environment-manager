4f4ac5ebe83612275bef52b48bf6fcd0b01a44e9
Michael Verhulst <michael@terminallabs.com>
2019-10-24 14:49:35 -0500

    Update install_python.sh

--- maintenance/general/conda/install_python.sh
+++ maintenance/general/conda/install_python.sh
@@ -8,6 +8,7 @@ su -m ${USERNAME} <<'EOF'
   unset SUDO_UID SUDO_GID SUDO_USER
   if [ $PLATFORM == "vagrant" ]; then
     touch home/${USERNAME}/.bashrc
+    HOME=/home/${USERNAME}
     LOGNAME=/home/${USERNAME}
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/vagrant/miniconda3
@@ -22,6 +23,7 @@ su -m ${USERNAME} <<'EOF'
     export PATH="/home/${USERNAME}/miniconda3/bin:$PATH"
   elif [ $PLATFORM == "mac" ]
   then
+    HOME=/Users/${USERNAME}
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
     bash Miniconda3-latest-MacOSX-x86_64.sh -b
     export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
