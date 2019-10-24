8ed799b00eaf091c1baa4a0d821ba73bb512e538
Michael Verhulst <michael@terminallabs.com>
2019-10-24 13:07:21 -0500

    Update install_cookiecutter.sh

--- maintenance/general/conda/install_cookiecutter.sh
+++ maintenance/general/conda/install_cookiecutter.sh
@@ -2,6 +2,10 @@ export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
 su -m vagrant <<'EOF'
+  USER=${USERNAME}
+  SUDO_USER=${USERNAME}
+  USERNAME=${USERNAME}
+  unset SUDO_UID SUDO_GID SUDO_USER  
   source maintenance/general/conda/env.sh
   pip install cookiecutter
   cookiecutter /vagrant --no-input
