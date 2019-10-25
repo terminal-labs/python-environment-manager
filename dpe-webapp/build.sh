d5abd90fc19159de76af0176ac39b6a745cc5be7
Michael Verhulst <michael@terminallabs.com>
2019-10-25 11:33:28 -0500

    Create build.sh

---
+++ dpe-webapp/build.sh
@@ -0,0 +1,18 @@
+export APPNAME=$1
+export USERNAME=$2
+export PLATFORM=$3
+su -m $USERNAME <<'EOF'
+  if [ $PLATFORM == "vagrant" ]; then
+    export USERNAME=vagrant
+    eval "vagrant up"
+    eval vagrant ssh --command 'cd /vagrant; sudo bash bootstrap.sh'
+  elif [ $PLATFORM == "linux" ]
+  then
+    echo "not implemented yet"
+  elif [ $PLATFORM == "mac" ]
+  then
+    echo "not implemented yet"
+  else
+    echo "not implemented yet"
+  fi
+EOF
