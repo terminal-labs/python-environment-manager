17f5d083fdc4348342b7743e576ae47d2514fcf4
Michael Verhulst <michael@terminallabs.com>
2020-01-05 15:10:55 -0600

    Update build.sh

--- maintenance/vagrant_saltstates/build.sh
+++ maintenance/vagrant_saltstates/build.sh
@@ -1,5 +1,5 @@
 vagrant up
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/install_deps.sh'
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/install_salt_open.sh'
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/run_salt.sh'
-vagrant ssh --command 'cd /vagrant; sudo bash maintenance/test.sh'
+vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/install_deps.sh'
+vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/install_salt_open.sh'
+vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/run_salt.sh'
+vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/test.sh'
