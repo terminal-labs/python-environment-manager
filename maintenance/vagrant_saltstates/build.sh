66ed9e75123508a395fca86fde8aafe86d53ac2e
Michael Verhulst <michael@terminallabs.com>
2020-01-05 16:27:06 -0600

    Update build.sh

--- maintenance/vagrant_saltstates/build.sh
+++ maintenance/vagrant_saltstates/build.sh
@@ -1,8 +1,8 @@
 export USERNAME=$1
 su -m $USERNAME <<'EOF'
-  vagrant up
-  vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/install_deps.sh'
-  vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/install_salt_open.sh'
-  vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/run_salt.sh'
-  vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/test.sh'
+  eval "vagrant up"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/install_deps.sh'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/install_salt_open.sh'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/run_salt.sh'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/test.sh'"
 EOF
