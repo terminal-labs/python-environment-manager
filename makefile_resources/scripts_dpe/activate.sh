5e9b6c0007cd83ae6b33e334ec288a6eaf1ed15f
Michael Verhulst <michael@terminallabs.com>
2020-01-13 00:21:59 -0600

    Update activate.sh

--- makefile_resources/scripts_dpe/activate.sh
+++ makefile_resources/scripts_dpe/activate.sh
@@ -1,7 +1,7 @@
 APPNAME=demo
 PYTHONVERSION=3.6.9
 DPENAME=dpe
-USERNAME=vagrant
+USERNAME=$1
 NODEVERSION=10.15.0
 SALTVERSION=v2018.3.3
 USER=USERNAME
@@ -22,23 +22,11 @@ LOGNAME=${USERNAME}  export PATH=/home/${USERNAME}/${DPENAME}/$APPNAME/miniconda
 export PATH=/home/${USERNAME}/${DPENAME}/$APPNAME/bin:$PATH
 export PATH=/home/${USERNAME}/${DPENAME}/$APPNAME/repos/lastpass-cli/build:$PATH
 
-export NVM_DIR=/home/${USERNAME}/${DPENAME}/$APPNAME/repos/nvm
 export USE_GIT_URI="true"
 
 source /home/${USERNAME}/.bashrc
-source /home/${USERNAME}/${DPENAME}/$APPNAME/repos/nvm/nvm.sh
 source activate ${APPNAME}
 
-echo "dpe v2019.8.0"
 conda --version
 python --version
-nvm --version
-node --version
-lpass --version
-salt --version
-terraform -version
 vagrant --version
-
-rambo --version
-inflation version
-utilitiespackage system version
