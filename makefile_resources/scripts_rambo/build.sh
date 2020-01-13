820d6d7b061fea1d6fc1af84086a7d11fd4929fc
Michael Verhulst <michael@terminallabs.com>
2020-01-13 14:25:39 -0600

    Update build.sh

--- makefile_resources/scripts_rambo/build.sh
+++ makefile_resources/scripts_rambo/build.sh
@@ -1,3 +1,14 @@
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+MACHINE=machine
+export MACHINE
+
 USERNAME=$1
 bash create.sh ${USERNAME}
 su -m ${USERNAME} <<'EOF'
