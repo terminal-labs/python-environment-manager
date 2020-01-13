daf49062d2812c4ec9dd247e42f8e2a2cfbb495f
Michael Verhulst <michael@terminallabs.com>
2020-01-12 23:56:55 -0600

    Update create.sh

--- makefile_resources/scripts_dpe/create.sh
+++ makefile_resources/scripts_dpe/create.sh
@@ -64,7 +64,7 @@ su -m ${USERNAME} <<'EOF'
   cd repos
   git clone https://github.com/nvm-sh/nvm.git
   git clone https://github.com/lastpass/lastpass-cli.git
-  git clone https://gitlab.com/terminallabs/utilitiespackage/utilities-package.git--recursive 
+  git clone https://gitlab.com/terminallabs/utilitiespackage/utilities-package.git --recursive 
   git clone https://github.com/terminal-labs/rambo.git --recursive
   git clone https://github.com/terminal-labs/inflation.git --recursiv    
   cd ..
