360afedcff73d56f389f27a4e08b742f9c481faf
Michael Verhulst <michael@terminallabs.com>
2020-01-14 13:30:09 -0600

    Update lib.sh

--- bash_scripts/lib.sh
+++ bash_scripts/lib.sh
@@ -1,8 +1,13 @@
-export USERNAME=$1
-su -m $USERNAME <<'EOF'
-  eval "vagrant up"
-  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/install_deps.sh'"
-  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/install_salt_open.sh'"
-  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/run_salt.sh'"
-  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/maintenance/vagrant_saltstates/test.sh'"
-EOF
+getmachine () {
+  unameOut="$(uname -s)"
+  case "${unameOut}" in
+      Linux*)     machine=Linux;;
+      Darwin*)    machine=Mac;;
+      CYGWIN*)    machine=Cygwin;;
+      MINGW*)     machine=MinGw;;
+      *)          machine="UNKNOWN:${unameOut}"
+  esac
+  MACHINE=$machine
+  export MACHINE
+  return MACHINE
+}
