c863942c5d974ca9e935aac1337a65f61d4b7a29
Michael Verhulst <michael@terminallabs.com>
2020-01-24 14:04:34 -0600

    Update build.sh

--- makefile_resources/scripts_python/conda/build.sh
+++ makefile_resources/scripts_python/conda/build.sh
@@ -5,21 +5,21 @@ su -m $USERNAME <<'EOF'
   if [ $PLATFORM == "vagrant" ]; then
     export USERNAME=vagrant
     eval "vagrant up"
-    eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/general/conda/install_apt_dependencies.sh $APPNAME $USERNAME $PLATFORM'"
-    eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/general/conda/install_python.sh $APPNAME $USERNAME $PLATFORM'"
-    eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/general/conda/install_pip_dependencies.sh $APPNAME $USERNAME $PLATFORM'"
-    eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/general/conda/install_app.sh $APPNAME $USERNAME $PLATFORM'"
-    eval "vagrant ssh --command 'cd /vagrant; sudo bash maintenance/general/conda/run_tests.sh $APPNAME $USERNAME $PLATFORM'"
+    eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/install_apt_dependencies.sh $APPNAME $USERNAME $PLATFORM'"
+    eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/install_python.sh $APPNAME $USERNAME $PLATFORM'"
+    eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/install_pip_dependencies.sh $APPNAME $USERNAME $PLATFORM'"
+    eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/install_app.sh $APPNAME $USERNAME $PLATFORM'"
+    eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/run_tests.sh $APPNAME $USERNAME $PLATFORM'"
   elif [ $PLATFORM == "linux" ]
   then
-    sudo bash maintenance/general/conda/install_python.sh $APPNAME $USERNAME $PLATFORM
-    sudo bash maintenance/general/conda/install_app.sh $APPNAME $USERNAME $PLATFORM
-    sudo bash maintenance/general/conda/run_tests.sh $APPNAME $USERNAME $PLATFORM
+    sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/install_python.sh $APPNAME $USERNAME $PLATFORM
+    sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/install_app.sh $APPNAME $USERNAME $PLATFORM
+    sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/run_tests.sh $APPNAME $USERNAME $PLATFORM
   elif [ $PLATFORM == "mac" ]
   then
-    sudo bash maintenance/general/conda/install_python.sh $APPNAME $USERNAME $PLATFORM
-    sudo bash maintenance/general/conda/install_app.sh $APPNAME $USERNAME $PLATFORM
-    sudo bash maintenance/general/conda/run_tests.sh $APPNAME $USERNAME $PLATFORM
+    sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/install_python.sh $APPNAME $USERNAME $PLATFORM
+    sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/install_app.sh $APPNAME $USERNAME $PLATFORM
+    sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/run_tests.sh $APPNAME $USERNAME $PLATFORM
   else
     echo "not implemented yet"
   fi
