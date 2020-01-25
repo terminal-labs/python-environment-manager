42826a8f58e8c6a7ada503875bc458bdc395538e
Michael Verhulst <michael@terminallabs.com>
2020-01-24 19:55:05 -0600

    Update build.sh

--- makefile_resources/scripts_dpe/build.sh
+++ makefile_resources/scripts_dpe/build.sh
@@ -3,7 +3,6 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   if [ $PLATFORM == "vagrant" ]; then
-    export USERNAME=vagrant
     eval "vagrant up"
     eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_dpe/create.sh $(APPNAME) $(USERNAME) vagrant"
   elif [ $PLATFORM == "linux" ]
