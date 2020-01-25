52e8bd8e60eba0c8c2285657c4fed0e53c16dd6c
Michael Verhulst <michael@terminallabs.com>
2020-01-24 19:58:39 -0600

    Update build.sh

--- makefile_resources/scripts_dpe/build.sh
+++ makefile_resources/scripts_dpe/build.sh
@@ -4,7 +4,7 @@ export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   if [ $PLATFORM == "vagrant" ]; then
     eval "vagrant up"
-    eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_dpe/create.sh $(APPNAME) $(USERNAME) vagrant"
+    eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_dpe/create.sh $(APPNAME) $(USERNAME) vagrant'"
   elif [ $PLATFORM == "linux" ]
   then
     echo "not implemented yet"
