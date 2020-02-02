190a469167f84d789d609e46fd1960b03e38455b
Michael Verhulst <michael@terminallabs.com>
2020-02-02 15:31:44 -0600

    Update run_tests.sh

--- makefile_resources/scripts_python/conda/run_tests.sh
+++ makefile_resources/scripts_python/conda/run_tests.sh
@@ -3,10 +3,15 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   source .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/env.sh
-  if [ -d $APPNAME ] && [ ! -z $APPNAME ];
+  if [ ! -f "setup.py" ];
   then
-    ${APPNAME} system version
-    ${APPNAME} system selftest
-    ${APPNAME} system selfcoverage
+    if [ -d $APPNAME ] && [ ! -z $APPNAME ];
+    then
+      ${APPNAME} system version
+      ${APPNAME} system selftest
+      ${APPNAME} system selfcoverage
+    fi
+  else
+    ${APPNAME} --version
   fi
 EOF
