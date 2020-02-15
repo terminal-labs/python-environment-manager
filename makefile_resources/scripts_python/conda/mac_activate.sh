92c881e15887ab9ef5aa7c37683625d806793240
Michael Verhulst <michael@terminallabs.com>
2020-02-14 21:45:29 -0600

    Update mac_activate.sh

--- makefile_resources/scripts_python/conda/mac_activate.sh
+++ makefile_resources/scripts_python/conda/mac_activate.sh
@@ -4,4 +4,4 @@ export PLATFORM=-platform-
 export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
 export PATH="/Users/$USERNAME/.local/bin:$PATH"
 export USE_GIT_URI="true"
-source activate ${APPNAME}
+conda activate ${APPNAME}
