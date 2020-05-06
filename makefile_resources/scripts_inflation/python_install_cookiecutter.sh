29b62eb057ab7745b8b0c77bbacaf60000a0cd5b
michael <michael@terminallabs.com>
2020-05-05 23:13:33 -0500

    added in some hybrid scripts

--- makefile_resources/scripts_inflation/python_install_cookiecutter.sh
+++ makefile_resources/scripts_inflation/python_install_cookiecutter.sh
@@ -6,7 +6,7 @@ su -m vagrant <<'EOF'
   SUDO_USER=${USERNAME}
   USERNAME=${USERNAME}
   unset SUDO_UID SUDO_GID SUDO_USER
-  source .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/env.sh
+  source .tmp/python-environment-manager-master/makefile_resources/scripts_inflation/python_env.sh
   pip install cookiecutter
   cookiecutter /vagrant --no-input
 EOF
--- makefile_resources/scripts_inflation/python_run_tests.sh
+++ makefile_resources/scripts_inflation/python_run_tests.sh
@@ -2,7 +2,7 @@ export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
-  source .tmp/python-environment-manager-master/makefile_resources/scripts_python/conda/env.sh
+  source .tmp/python-environment-manager-master/makefile_resources/scripts_inflation/python_env.sh
   if [ ! -f "setup.py" ];
   then
     if [ -d $APPNAME ] && [ ! -z $APPNAME ];
