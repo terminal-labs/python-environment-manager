9270e2f4f1444da315284f4de4d685b3d8fe5826
Michael Verhulst <michael@terminallabs.com>
2020-05-06 13:48:43 -0500

    Update build.sh

--- makefile_resources/scripts_inflation/build.sh
+++ makefile_resources/scripts_inflation/build.sh
@@ -11,7 +11,9 @@ su -m $USERNAME <<'EOF'
     echo "not implemented yet"
   elif [ $PLATFORM == "mac" ]
   then
-    echo "not implemented yet"
+    export USERNAME="vagrant"
+    eval "vagrant up"
+    eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_inflation/create.sh $APPNAME $USERNAME $PLATFORM'"
   else
     echo "not implemented yet"
   fi
