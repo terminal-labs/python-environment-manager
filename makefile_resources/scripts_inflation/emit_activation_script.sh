535f60922405a2acd99a60b13bf6f11dd6bf2de5
Michael Verhulst <michael@terminallabs.com>
2020-05-06 20:12:38 -0500

    Update emit_activation_script.sh

--- makefile_resources/scripts_inflation/emit_activation_script.sh
+++ makefile_resources/scripts_inflation/emit_activation_script.sh
@@ -2,16 +2,18 @@ export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME << 'EOF'
+  mkdir -p .internal
   if [ $PLATFORM == "linux" ]; then
-    cp .tmp/python-environment-manager-master/makefile_resources/scripts_dpe/linux_activate.sh activate.sh
+    cp .tmp/python-environment-manager-master/makefile_resources/scripts_inflation/linux_activate.sh .internal/activate.sh
   elif [ $PLATFORM == "mac" ]; then
-    cp .tmp/python-environment-manager-master/makefile_resources/scripts_dpe/mac_activate.sh activate.sh
+    cp .tmp/python-environment-manager-master/makefile_resources/scripts_inflation/mac_activate.sh .internal/activate.sh
   elif [ $PLATFORM == "vagrant" ]; then
     export USERNAME=vagrant
-    cp .tmp/python-environment-manager-master/makefile_resources/scripts_dpe/linux_activate.sh activate.sh
+    cp .tmp/python-environment-manager-master/makefile_resources/scripts_inflation/linux_activate.sh .internal/activate.sh
   else
     echo "not implemented yet"
   fi
+  cd .internal
   sed "s/-appname-/$APPNAME/g" activate.sh > activate.tmp
   rm activate.sh
   mv activate.tmp activate.sh
@@ -21,4 +23,5 @@ su -m $USERNAME << 'EOF'
   sed "s/-platform-/$PLATFORM/g" activate.sh > activate.tmp
   rm activate.sh
   mv activate.tmp activate.sh
+ cd -
 EOF
