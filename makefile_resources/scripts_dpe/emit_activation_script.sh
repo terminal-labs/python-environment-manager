6789c7a94224b719525202f512ceb1a3c621cdf5
Michael Verhulst <michael@terminallabs.com>
2020-05-14 20:00:02 -0500

    Update emit_activation_script.sh

--- makefile_resources/scripts_dpe/emit_activation_script.sh
+++ makefile_resources/scripts_dpe/emit_activation_script.sh
@@ -3,10 +3,10 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME << 'EOF'
   if [ $PLATFORM == "linux" ]; then
-    cp .tmp/python-environment-manager-master/makefile_resources/scripts_dpe/linux_activate.sh activate.sh
+    cp .tmp/bash-environment-manager-master/makefile_resources/scripts_dpe/linux_activate.sh activate.sh
   elif [ $PLATFORM == "mac" ]
   then
-    cp .tmp/python-environment-manager-master/makefile_resources/scripts_dpe/mac_activate.sh activate.sh
+    cp .tmp/bash-environment-manager-master/makefile_resources/scripts_dpe/mac_activate.sh activate.sh
    else
     echo "not implemented yet"
   fi
