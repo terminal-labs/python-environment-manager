7f5416748cbcd70d703b5bfcdf6c98c39788552a
Michael Verhulst <michael@terminallabs.com>
2020-05-20 00:25:22 -0500

    Update run_tests.sh

--- makefile_resources/scripts_python/run_tests.sh
+++ makefile_resources/scripts_python/run_tests.sh
@@ -3,15 +3,10 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   source .tmp/bash-environment-manager-master/makefile_resources/scripts_python/env.sh
-  if [ ! -f "setup.py" ];
+  if [ -f ".repo/tests" ];
   then
-    if [ -f ".repo/tests" ];
-    then
-      ${APPNAME} system version
-      ${APPNAME} system selftest
-      ${APPNAME} system selfcoverage
-    fi
-  else
-    ${APPNAME} --version
+    ${APPNAME} system version
+    ${APPNAME} system selftest
+    ${APPNAME} system selfcoverage
   fi
 EOF
