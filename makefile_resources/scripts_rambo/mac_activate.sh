39d56e198a02ef172cdf223733cd5102e0d053c4
Michael Verhulst <michael@terminallabs.com>
2020-07-17 17:29:23 -0500

    Update mac_activate.sh

--- makefile_resources/scripts_rambo/mac_activate.sh
+++ makefile_resources/scripts_rambo/mac_activate.sh
@@ -1,7 +1,17 @@
 export APPNAME=-appname-
 export USERNAME=-username-
 export PLATFORM=-platform-
-export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
-export PATH="/Users/$USERNAME/.local/bin:$PATH"
+unset SUDO_UID SUDO_GID SUDO_USER
+USER=${USERNAME}
+SUDO_USER=${USERNAME}
+USERNAME=${USERNAME}
+HOME=/Users/${USERNAME}
+LOGNAME=${USERNAME}
+export PATH=/Users/${USERNAME}/dpe/$APPNAME/miniconda3/bin:$PATH
+export PATH=/Users/${USERNAME}/dpe/$APPNAME/bin:$PATH
+export PATH=/Users/${USERNAME}/dpe/$APPNAME/repos/lastpass-cli/build:$PATH
 export USE_GIT_URI="true"
+source /Users/${USERNAME}/.bashrc
 source activate ${APPNAME}
+conda --version
+python --version
