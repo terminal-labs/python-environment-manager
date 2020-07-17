6704e9f880b489502041bc1746a320284f28fbd1
Michael Verhulst <michael@terminallabs.com>
2020-07-17 12:56:53 -0500

    Update create.sh

--- makefile_resources/scripts_rambo/create.sh
+++ makefile_resources/scripts_rambo/create.sh
@@ -9,7 +9,7 @@ export DPENAME
 export USERNAME
 export USER
 
-source .tmp/python-environment-manager-master/bash_scripts/lib.sh
+source .tmp/bash-environment-manager-master/bash_scripts/lib.sh
 getmachine
 MACHINE=$_MACHINE
 export MACHINE
