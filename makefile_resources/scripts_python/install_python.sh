ad531de60b1eca13878c35a8dda94d71c12c92fa
Michael Verhulst <michael@terminallabs.com>
2020-11-21 01:02:00 -0600

    Update install_python.sh

--- makefile_resources/scripts_python/install_python.sh
+++ makefile_resources/scripts_python/install_python.sh
@@ -6,7 +6,39 @@ su -m ${USERNAME} <<'EOF'
   SUDO_USER=${USERNAME}
   USERNAME=${USERNAME}
   unset SUDO_UID SUDO_GID SUDO_USER
-  
+
+  if [ $PLATFORM == "vagrant" ]; then
+    HOME=/home/${USERNAME}	  source .tmp/bash-environment-manager-master/makefile_resources/scripts_python/env.sh
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p /home/vagrant/miniconda3
+    export PATH="/home/vagrant/miniconda3/bin:$PATH"
+    source /home/vagrant/miniconda3/etc/profile.d/conda.sh;
+  elif [ $PLATFORM == "linux" ]
+  then
+    HOME=/home/${USERNAME}
+    if [ ! -d "/home/${USERNAME}/miniconda3" ]; then
+      wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+      bash Miniconda3-latest-Linux-x86_64.sh -b
+    fi
+    export PATH="/home/${USERNAME}/miniconda3/bin:$PATH"
+    source /home/${USERNAME}/miniconda3/etc/profile.d/conda.sh;
+  elif [ $PLATFORM == "mac" ]
+  then
+    HOME=/Users/${USERNAME}
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    bash Miniconda3-latest-MacOSX-x86_64.sh -b
+    export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
+    source /Users/${USERNAME}/miniconda3/etc/profile.d/conda.sh;
+  else
+    echo "not implemented yet"
+  fi
+  if [ -f "Miniconda3-latest-Linux-x86_64.sh" ]; then
+    rm Miniconda3-latest*
+  fi
+  if [ -f "Miniconda3-latest-MacOSX-x86_64.sh" ]; then
+    rm Miniconda3-latest*
+  fi
+
   source .tmp/bash-environment-manager-master/makefile_resources/scripts_python/env.sh
 
   conda --version
