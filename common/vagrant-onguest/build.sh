5d0e6e4bc1f78a15826197812c6c0b5120193fae
Michael Verhulst <michael@terminallabs.com>
2020-11-23 15:33:57 -0600

    Update build.sh

--- common/vagrant-onguest/build.sh
+++ common/vagrant-onguest/build.sh
@@ -3,5 +3,5 @@ export USERNAME=$2
 export PLATFORM=$3
 su -m $USERNAME <<'EOF'
   eval "vagrant up"
-  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/bash-environment-manager-master/common/onguest/build.sh'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/bash-environment-manager-master/common/onguest/build.sh"" ${APPNAME}"" ${USERNAME}"" ${PLATFORM}'"
 EOF
