4d3a4067d416d77070300775692b9843fdfe8b2b
Michael Verhulst <michael@terminallabs.com>
2020-11-23 13:33:55 -0600

    Update build.sh

--- common/vagrant-onguest/build.sh
+++ common/vagrant-onguest/build.sh
@@ -1,20 +1,10 @@
 export APPNAME=$1
 export USERNAME=$2
-
-source .tmp/bash-environment-manager-master/bash_scripts/lib.sh
-getmachine
-MACHINE=$_MACHINE
-export MACHINE
-
-bash .tmp/bash-environment-manager-master/makefile_resources/scripts_inflation/create.sh ${APPNAME} ${USERNAME}
-su -m ${USERNAME} <<'EOF'
-  source .tmp/bash-environment-manager-master/makefile_resources/scripts_inflation/activate.sh ${APPNAME} ${USERNAME}
-
-  FILE=auth/env.sh
-  if test -f "$FILE"; then
-    . auth/env.sh
-  fi
-
-  inflation inflate z_inflation.conf
-
+export PLATFORM=$3
+su -m $USERNAME <<'EOF'
+  eval "vagrant up"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_vagrant_saltstates/install_deps.sh'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_vagrant_saltstates/install_salt_open.sh'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_vagrant_saltstates/run_salt.sh'"
+  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/python-environment-manager-master/makefile_resources/scripts_vagrant_saltstates/test.sh'"
 EOF
