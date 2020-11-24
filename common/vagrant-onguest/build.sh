08c1ea8382ad312c32d0fce9169264eb341fae46
verhulstm <michael@terminallabs.com>
2020-11-23 23:15:31 -0600

    cleaning

--- common/vagrant-onguest/build.sh
+++
@@ -1,7 +0,0 @@
-export APPNAME=$1
-export USERNAME=$2
-export PLATFORM=$3
-su -m $USERNAME <<'EOF'
-  eval "vagrant up"
-  eval "vagrant ssh --command 'cd /vagrant; sudo bash .tmp/bash-environment-manager-master/common/onguest/build.sh"" ${APPNAME}"" vagrant"" ${PLATFORM}'"
-EOF
--- scripts/dpe/assemble.sh
+++ scripts/dpe/assemble.sh
@@ -1,6 +1,8 @@
 export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
+export CMD=$4
+
 if [ $PLATFORM == "vagrant" ]; then
-  bash .tmp/bash-environment-manager-master/common/vagrant-onguest/build.sh $APPNAME $SUDO_USER vagrant
+  bash .tmp/bash-environment-manager-master/common/vagrant-onguest/deploy.sh $APPNAME $USERNAME vagrant $CMD
 fi
