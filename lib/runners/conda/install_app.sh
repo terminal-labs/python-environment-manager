df04871d5bd363f9ea416e74cb2486b0af167762
Michael Verhulst <michael@terminallabs.com>
2020-11-26 17:59:59 -0600

    Update install_app.sh

--- lib/runners/conda/install_app.sh
+++ lib/runners/conda/install_app.sh
@@ -1,24 +1,37 @@
 export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
-su -m ${USERNAME} <<'EOF'
-  USER=${USERNAME}
-  SUDO_USER=${USERNAME}
-  USERNAME=${USERNAME}
-  unset SUDO_UID SUDO_GID SUDO_USER
+export PYTHONVERSION=$4
+export DPENAME=$5
+export USER=$6
+export USERHOME=$7
+export MACHINE=$8
+export CMD=$9
 
-  source .tmp/bash-environment-manager-master/makefile_resources/scripts_python/env.sh
+unset SUDO_UID SUDO_GID SUDO_USER
 
-  conda activate ${APPNAME}
+USER=$USERNAME
+SUDO_USER=$USERNAME
+USERNAME=$USERNAME
+LOGNAME=$USERNAME
 
-  if [ -f "setup.py" ];
+if [ $MACHINE == "Mac" ]; then
+  export PATH="/Users/$USERNAME/miniconda3/bin:$PATH"
+  source /Users/$USERNAME/miniconda3/etc/profile.d/conda.sh;
+else
+  export PATH="/home/$USERNAME/miniconda3/bin:$PATH"
+  source /home/$USERNAME/miniconda3/etc/profile.d/conda.sh;
+fi
+
+conda activate ${APPNAME}
+
+if [ -f "setup.py" ];
+then
+  pip install -e .
+else
+  if [ -d $APPNAME ] && [ ! -z $APPNAME ];
   then
+    cd $APPNAME
     pip install -e .
-  else
-    if [ -d $APPNAME ] && [ ! -z $APPNAME ];
-    then
-      cd $APPNAME
-      pip install -e .
-    fi
   fi
-EOF
+fi
