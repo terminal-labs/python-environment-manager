1ea6c2ee28ae6f0a2c00134bb8dfde096c9f1d2c
Michael Verhulst <michael@terminallabs.com>
2020-11-26 19:02:22 -0600

    Update test_run.sh

--- lib/runners/conda/test_run.sh
+++ lib/runners/conda/test_run.sh
@@ -1,26 +1,39 @@
 export APPNAME=$1
 export USERNAME=$2
 export PLATFORM=$3
-su -m ${USERNAME} <<'EOF'
-  USER=${USERNAME}
-  SUDO_USER=${USERNAME}
-  USERNAME=${USERNAME}
-  unset SUDO_UID SUDO_GID SUDO_USER
+export PYTHONVERSION=$4
+export DPENAME=$5
+export USER=$6
+export USERHOME=$7
+export MACHINE=$8
+export CMD=$9
 
-  source .tmp/bash-environment-manager-master/makefile_resources/scripts_python/env.sh
+unset SUDO_UID SUDO_GID SUDO_USER
 
-  conda activate ${APPNAME}
+USER=$USERNAME
+SUDO_USER=$USERNAME
+USERNAME=$USERNAME
+LOGNAME=$USERNAME
 
-  if [ -f ".repo/tests" ];
-  then
-    if jumper >/dev/null 2>&1; then
-      jumper system version
-      jumper system selftest
-      jumper system selfcoverage
-    else
-      ${APPNAME} system version
-      ${APPNAME} system selftest
-      ${APPNAME} system selfcoverage
-    fi
+if [ $MACHINE == "Mac" ]; then
+  export PATH="/Users/$USERNAME/miniconda3/bin:$PATH"
+  source /Users/$USERNAME/miniconda3/etc/profile.d/conda.sh;
+else
+  export PATH="/home/$USERNAME/miniconda3/bin:$PATH"
+  source /home/$USERNAME/miniconda3/etc/profile.d/conda.sh;
+fi
+
+conda activate ${APPNAME}
+
+if [ -f ".repo/tests" ];
+then
+  if jumper >/dev/null 2>&1; then
+    jumper system version
+    jumper system selftest
+    jumper system selfcoverage
+  else
+    ${APPNAME} system version
+    ${APPNAME} system selftest
+    ${APPNAME} system selfcoverage
   fi
-EOF
+fi
