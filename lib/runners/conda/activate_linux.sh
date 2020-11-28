41c5768a571a740ad33001f292baad679064185b
Michael Verhulst <michael@terminallabs.com>
2020-11-27 18:47:12 -0600

    Update activate_linux.sh

--- lib/runners/conda/activate_linux.sh
+++ lib/runners/conda/activate_linux.sh
@@ -1,9 +1,7 @@
 export APPNAME=-appname-
 export USERNAME=-username-
-export PLATFORM=-platform-
-export PATH="/home/${USERNAME}/miniconda3/bin:$PATH"
-export PATH="/home/$USERNAME/.local/bin:$PATH"
-export USE_GIT_URI="true"
-source /home/${USERNAME}/.bashrc
-source /home/${USERNAME}/miniconda3/etc/profile.d/conda.sh;
+
+export PATH="/home/$USERNAME/miniconda3/bin:$PATH"
+source /home/$USERNAME/miniconda3/etc/profile.d/conda.sh;
+
 conda activate ${APPNAME}
