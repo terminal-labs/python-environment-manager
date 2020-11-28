9c444810c64a8a471fd3b8746b4167b2177ed0d2
verhulstm <michael@terminallabs.com>
2020-11-27 18:21:48 -0600

    added in activate script

--- lib/runners/conda/activate_mac.sh
+++ lib/runners/conda/activate_mac.sh
@@ -1,9 +1,7 @@
 export APPNAME=-appname-
 export USERNAME=-username-
-export PLATFORM=-platform-
-export PATH="/Users/${USERNAME}/miniconda3/bin:$PATH"
-export PATH="/Users/$USERNAME/.local/bin:$PATH"
-export USE_GIT_URI="true"
-source /Users/${USERNAME}/.bash_profile
-source /Users/${USERNAME}/miniconda3/etc/profile.d/conda.sh;
+
+export PATH="/Users/$USERNAME/miniconda3/bin:$PATH"
+source /Users/$USERNAME/miniconda3/etc/profile.d/conda.sh;
+
 conda activate ${APPNAME}
--- lib/runners/conda/emit_activation.sh
+++ lib/runners/conda/emit_activation.sh
@@ -1,22 +1,18 @@
-export APPNAME=$1
-export USERNAME=$2
-export PLATFORM=$3
-su -m $USERNAME << 'EOF'
-  if [ $PLATFORM == "linux" ]; then
-    cp .tmp/bash-environment-manager-master/makefile_resources/scripts_python/linux_activate.sh activate.sh
-  elif [ $PLATFORM == "mac" ]
-  then
-    cp .tmp/bash-environment-manager-master/makefile_resources/scripts_python/mac_activate.sh activate.sh
-   else
-    echo "not implemented yet"
-  fi
-  sed "s/-appname-/$APPNAME/g" activate.sh > activate.tmp
-  rm activate.sh
-  mv activate.tmp activate.sh
-  sed "s/-username-/$USERNAME/g" activate.sh > activate.tmp
-  rm activate.sh
-  mv activate.tmp activate.sh
-  sed "s/-platform-/$PLATFORM/g" activate.sh > activate.tmp
-  rm activate.sh
-  mv activate.tmp activate.sh
-EOF
+source .tmp/bash-environment-manager-master/lib/bash/lib.sh
+getmachine
+MACHINE=$_MACHINE
+export MACHINE
+
+if [ $MACHINE == "Mac" ]; then
+  cp .tmp/bash-environment-manager-master/lib/runners/conda/activate_mac.sh activate.sh
+else
+  cp .tmp/bash-environment-manager-master/lib/runners/conda/activate_linux.sh activate.sh
+fi
+
+sed "s/-appname-/$APPNAME/g" activate.sh > activate.tmp
+rm activate.sh
+mv activate.tmp activate.sh
+
+sed "s/-username-/$USERNAME/g" activate.sh > activate.tmp
+rm activate.sh
+mv activate.tmp activate.sh
