6f8847d9361bbee3b7b785a225e08a142db688a6
Michael Verhulst <michael@terminallabs.com>
2020-11-29 00:02:57 -0600

    Update init.sh

--- lib/runners/saltstates/init.sh
+++ lib/runners/saltstates/init.sh
@@ -1,8 +1,13 @@
+mkdir -o /vagrant
+mkdir -p /vagrant/.tmp
+
+cd /vagrant/.tmp
+
 wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
 sh bootstrap-salt.sh -P -X git v2018.3.3
 rm bootstrap-salt.sh
 
-mkdir -p /etc
-mkdir -p /etc/salt
-rm -rf /etc/salt/minion
-cp salt/minion /etc/salt/minion
+cd /vagrant
+
+cp -r saltstack/etc/* /etc/salt
+cp -r saltstack/srv/* /srv
