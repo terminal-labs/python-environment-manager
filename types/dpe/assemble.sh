0bbe1c7a17c8b141324ebe2c8bc07c11cce0d63f
Michael Verhulst <michael@terminallabs.com>
2020-11-29 21:14:25 -0600

    Update assemble.sh

--- types/dpe/assemble.sh
+++ types/dpe/assemble.sh
@@ -3,14 +3,14 @@ export USERNAME=$2
 export PLATFORM=$3
 export CMD=$4
 
-if [ $CMD == "onhost" ]; then
+if [[ $CMD == "onhost" ]]; then
   bash .tmp/bash-environment-manager-master/common/onhost/deploy.sh $APPNAME $USERNAME computed $CMD
 fi
 
-if [ $CMD == "onguest" ]; then
+if [[ $CMD == "onguest" ]]; then
   bash .tmp/bash-environment-manager-master/common/onguest/deploy.sh $APPNAME $USERNAME computed $CMD
 fi
 
-if [ $CMD == "vagrant-onguest" ]; then
+if [[ $CMD == "vagrant-onguest" ]]; then
   bash .tmp/bash-environment-manager-master/common/vagrant-onguest/deploy.sh $APPNAME $USERNAME vagrant $CMD
 fi
