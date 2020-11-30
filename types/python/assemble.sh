7df6266b1483a5869d7a29ad186d832871bc2285
verhulstm <michael@terminallabs.com>
2020-11-29 21:26:14 -0600

    adding [[ not [

--- types/python/assemble.sh
+++ types/python/assemble.sh
@@ -3,10 +3,10 @@ export USERNAME=$2
 export PLATFORM=$3
 export CMD=$4
 
-if [ $CMD == "conda" ]; then
+if [[ $CMD == "conda" ]]; then
   bash .tmp/bash-environment-manager-master/common/conda/deploy.sh $APPNAME $USERNAME computed $CMD
 fi
 
-if [ $CMD == "vagrant-conda" ]; then
+if [[ $CMD == "vagrant-conda" ]]; then
   bash .tmp/bash-environment-manager-master/common/vagrant-conda/deploy.sh $APPNAME $USERNAME computed $CMD
 fi
--- types/saltstates/assemble.sh
+++ types/saltstates/assemble.sh
@@ -3,10 +3,10 @@ export USERNAME=$2
 export PLATFORM=$3
 export CMD=$4
 
-if [ $CMD == "saltstates" ]; then
+if [[ $CMD == "saltstates" ]]; then
   bash .tmp/bash-environment-manager-master/common/saltstates/deploy.sh $APPNAME $USERNAME computed $CMD
 fi
 
-if [ $CMD == "vagrant-saltstates" ]; then
+if [[ $CMD == "vagrant-saltstates" ]]; then
   bash .tmp/bash-environment-manager-master/common/vagrant-saltstates/deploy.sh $APPNAME $USERNAME computed $CMD
 fi
