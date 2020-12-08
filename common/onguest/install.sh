a10d857486c7252ff06643f866738b9d63e8dd4e
Michael Verhulst <michael@terminallabs.com>
2020-12-07 23:35:16 -0600

    Update install.sh

--- common/onguest/install.sh
+++ common/onguest/install.sh
@@ -22,22 +22,22 @@ if [[ $MACHINE != "Mac" ]]; then
   bash .tmp/bash-environment-manager-master/lib/deps/apt.sh
 fi
 
-su -mp $USERNAME <<'EOF'
+su -m $USERNAME <<'EOF'
   bash .tmp/bash-environment-manager-master/lib/runners/dpe/init.sh $APPNAME $USERNAME $PLATFORM $PYTHONVERSION $DPENAME $USER $USERHOME $MACHINE $CMD
 EOF
 
-su -mp $USERNAME <<'EOF'
+su -m $USERNAME <<'EOF'
   bash .tmp/bash-environment-manager-master/lib/runners/dpe/install_vagrant.sh $APPNAME $USERNAME $PLATFORM $PYTHONVERSION $DPENAME $USER $USERHOME $MACHINE $CMD
 EOF
 
-su -mp $USERNAME <<'EOF'
+su -m $USERNAME <<'EOF'
   bash .tmp/bash-environment-manager-master/lib/runners/dpe/install_salt.sh $APPNAME $USERNAME $PLATFORM $PYTHONVERSION $DPENAME $USER $USERHOME $MACHINE $CMD
 EOF
 
-su -mp $USERNAME <<'EOF'
+su -m $USERNAME <<'EOF'
   sudo bash .tmp/bash-environment-manager-master/lib/runners/dpe/insert_salt.sh $APPNAME $USERNAME $PLATFORM $PYTHONVERSION $DPENAME $USER $USERHOME $MACHINE $CMD
 EOF
 
-su -mp $USERNAME <<'EOF'
+su -m $USERNAME <<'EOF'
   sudo bash .tmp/bash-environment-manager-master/lib/runners/dpe/run_salt.sh $APPNAME $USERNAME $PLATFORM $PYTHONVERSION $DPENAME $USER $USERHOME $MACHINE $CMD
 EOF
