0b0257faa011cf29329f1e2c0de715424de90154
Michael Verhulst <michael@terminallabs.com>
2020-12-08 01:42:46 -0600

    Update run_salt.sh

--- lib/runners/dpe/run_salt.sh
+++ lib/runners/dpe/run_salt.sh
@@ -19,6 +19,6 @@ export PATH=$USERHOME/$DPENAME/$APPNAME/bin:$PATH
 source activate $APPNAME
 
 salt-call \
-   --log-level=info \
+   --log-level=debug \
    --state-output=terse \
    --local state.highstate
