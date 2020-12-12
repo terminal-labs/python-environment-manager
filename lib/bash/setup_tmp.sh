644f5a05b49dbf7f4dd72ae86600081114f387b6
Michael Verhulst <michael@terminallabs.com>
2020-12-11 19:50:49 -0600

    Update setup_tmp.sh

--- lib/bash/setup_tmp.sh
+++ lib/bash/setup_tmp.sh
@@ -1,1 +1,13 @@
-
+mkdir -p .tmp
+cd .tmp
+mkdir -p downloads
+mkdir -p artifacts
+mkdir -p unzipped
+mkdir -p scratch
+mkdir -p scripts
+mkdir -p clones
+mkdir -p repos
+mkdir -p auth
+mkdir -p data
+mkdir -p bin
+cd -
