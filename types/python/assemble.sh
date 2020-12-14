de27637f16decaaec71072a6333a6f537d298470
Michael Verhulst <michael@terminallabs.com>
2020-12-13 21:27:16 -0600

    Update assemble.sh

--- types/python/assemble.sh
+++ types/python/assemble.sh
@@ -3,6 +3,10 @@ export USERNAME=$2
 export PLATFORM=$3
 export CMD=$4
 
+su -m $USERNAME <<'EOF'
+  bash .tmp/bash-environment-manager-master/lib/bash/setup_tmp.sh
+EOF
+
 if [[ $CMD == "conda" ]]; then
   bash .tmp/bash-environment-manager-master/common/conda/deploy.sh $APPNAME $USERNAME computed $CMD
 fi
