09a1b81314832add5107e4fa0aaed06b55097daf
verhulstm <michael@terminallabs.com>
2020-12-21 17:42:04 -0600

    cleaning

--- lib/runners/dpe/run_salt.sh
+++ lib/runners/dpe/run_salt.sh
@@ -20,7 +20,17 @@ sudo su -m root <<'EOF'
 if [[ -d "saltstack" ]]; then
   export PATH=/application/platform/miniconda3/bin:$PATH
   export PATH=/home/vagrant/.local/bin:$PATH
-  source activate sdm
-  salt-call --log-level=info --state-output=terse --local state.highstate
+  source activate $APPNAME
+
+  export DEESCALATED_SALT_CONFIG_DIR=/Users/mike/Desktop/bash-environment-templates/samples/fuzzball/saltstack/etc
+  export DEESCALATED_SALT_ROOT_DIR=/Users/mike/Desktop/bash-environment-templates/samples/fuzzball/saltstack/srv/salt
+  export DEESCALATED_SALT_LOG_FILE=/Users/mike/Desktop/bash-environment-templates/samples/fuzzball/saltstack/logs/logs
+
+  salt-call \
+     --config-dir=$DEESCALATED_SALT_CONFIG_DIR \
+     --log-file=$DEESCALATED_SALT_LOG_FILE \
+     --file-root=$DEESCALATED_SALT_ROOT_DIR \
+     --log-level=info \
+     --local state.highstate
 fi
 EOF
