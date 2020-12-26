658862a565cf19dc5f88ccd89a7f14a9af45f1de
Michael Verhulst <michael@terminallabs.com>
2020-12-26 12:59:49 -0600

    Update assemble.sh

--- types/python/assemble.sh
+++ types/python/assemble.sh
@@ -10,11 +10,15 @@ getuserhome
 emit_env_file
 
 edit_env_file "-appname-" $APPNAME
+edit_env_file "-username-" $USERNAME
 edit_env_file "-platform-" $PLATFORM
 edit_env_file "-cmd-" $CMD
+edit_env_file "-machine-" $MACHINE
 edit_env_file "-userhome-" $USERHOME
 edit_env_file "-pythonversion-" "3.6.9"
 edit_env_file "-dpename-" "dpe"
+edit_env_file "-user-" $USERNAME
+edit_env_file "-wd-" $(pwd)
 
 su -m $USERNAME <<'EOF'
   bash .tmp/bash-environment-manager-master/lib/bash/setup_tmp.sh
