fb5ec972d8c6a4148d8fb912971219c7fa728df4
Michael Verhulst <michael@terminallabs.com>
2020-12-26 02:14:33 -0600

    Update init.sh

--- lib/runners/conda/init.sh
+++ lib/runners/conda/init.sh
@@ -1,23 +1,4 @@
 source .tmp/_env.sh
 
 source .tmp/bash-environment-manager-master/lib/bash/setup_miniconda.sh
-
-export PATH=$PLATFORM/platform/miniconda3/bin:$PATH
-
-conda --version
-conda init bash
-
-source $PLATFORM/platform/miniconda3/etc/profile.d/conda.sh
-
-conda update -y -n base -c defaults conda
-conda --version
-conda update -y conda
-conda --version
-conda create -y -n $APPNAME python=$PYTHONVERSION
-
-conda activate $APPNAME
-
-pip install --upgrade pip
-pip install setuptools==45
-
-pip install pyyaml
+source .tmp/bash-environment-manager-master/lib/bash/setup_miniconda_env.sh
