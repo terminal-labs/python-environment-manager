8b1c3906e4c7cf55669f0b1a235207786e1109a9
verhulstm <michael@terminallabs.com>
2020-12-26 15:59:56 -0600

    added emit to interface

--- types/interface/emit/activate_linux.sh
+++ types/interface/emit/activate_linux.sh
@@ -1,7 +1,6 @@
-export APPNAME=-appname-
-export USERNAME=-username-
+source .tmp/_env.sh
 
-export PATH="/home/$USERNAME/miniconda3/bin:$PATH"
-source /home/$USERNAME/miniconda3/etc/profile.d/conda.sh;
+export PATH=$PLATFORM/platform/miniconda3/bin:$PATH
+source $PLATFORM/platform/miniconda3/etc/profile.d/conda.sh
 
-conda activate ${APPNAME}
+conda activate $APPNAME
