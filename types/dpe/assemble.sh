e744da85207f4df815b45b6d52a4161d65c615f1
Michael Verhulst <michael@terminallabs.com>
2021-01-24 16:41:43 -0600

    Update assemble.sh

--- types/dpe/assemble.sh
+++ types/dpe/assemble.sh
@@ -1,6 +1,7 @@
 export APPNAME=$1
 export USERNAME=$2
-export CMD=$3
+export PLATFORM=$3
+export CMD=$4
 
 source .tmp/bash-environment-manager-master/lib/bash/vars.sh
 source .tmp/bash-environment-manager-master/lib/bash/lib.sh
@@ -10,14 +11,12 @@ getuserhome
 emit_env_file
 
 edit_env_file "-appname-" $APPNAME
-edit_env_file "-username-" $USERNAME
 edit_env_file "-platform-" $PLATFORM
 edit_env_file "-cmd-" $CMD
 edit_env_file "-machine-" $MACHINE
 edit_env_file "-userhome-" $USERHOME
 edit_env_file "-pythonversion-" "3.6.9"
 edit_env_file "-dpename-" "dpe"
-edit_env_file "-user-" $USERNAME
 edit_env_file "-wd-" $(pwd)
 
 su -m $USERNAME <<'EOF'
@@ -25,13 +24,22 @@ su -m $USERNAME <<'EOF'
 EOF
 
 if [[ $CMD == "onhost" ]]; then
+  edit_env_file "-user-" $USERNAME
+  edit_env_file "-username-" $USERNAME
+  chown $USERNAME .tmp/_env.sh
   bash .tmp/bash-environment-manager-master/common/onhost/deploy.sh
 fi
 
 if [[ $CMD == "onguest" ]]; then
+  edit_env_file "-user-" $USERNAME
+  edit_env_file "-username-" $USERNAME
+  chown $USERNAME .tmp/_env.sh
   bash .tmp/bash-environment-manager-master/common/onguest/deploy.sh
 fi
 
 if [[ $CMD == "vagrant-onguest" ]]; then
+  edit_env_file "-user-" vagrant
+  edit_env_file "-username-" $USERNAME
+  chown $USERNAME .tmp/_env.sh
   bash .tmp/bash-environment-manager-master/common/vagrant-onguest/deploy.sh
 fi
