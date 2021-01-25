1b0a301a463d19f7dae35ec1bd7b38eeafef1e9c
Michael Verhulst <michael@terminallabs.com>
2021-01-24 21:30:07 -0600

    Update run_salt.sh

--- lib/runners/dpe/run_salt.sh
+++ lib/runners/dpe/run_salt.sh
@@ -6,15 +6,15 @@ if [[ -d "saltstack" ]]; then
   export PATH=$PLATFORM/platform/miniconda3/bin:$PATH
   source $PLATFORM/platform/miniconda3/etc/profile.d/conda.sh
   source activate $APPNAME
-  export DEESCALATED_SALT_CONFIG_DIR=/home/user/Desktop/bash-environment-templates/samples/fuzzball/saltstack/etc
-  export DEESCALATED_SALT_ROOT_DIR=/home/user/Desktop/bash-environment-templates/samples/fuzzball/saltstack/srv/salt
-  export DEESCALATED_SALT_LOG_FILE=/home/user/Desktop/bash-environment-templates/samples/fuzzball/fuzzball/saltstack/logs/logs
+  export DEESCALATED_SALT_CONFIG_DIR=$WD/saltstack/etc
+  export DEESCALATED_SALT_ROOT_DIR=$WD/saltstack/srv/salt
+  export DEESCALATED_SALT_LOG_FILE=$WD/saltstack/logs/logs
   salt-call \
      --config-dir=$DEESCALATED_SALT_CONFIG_DIR \
      --log-file=$DEESCALATED_SALT_LOG_FILE \
      --file-root=$DEESCALATED_SALT_ROOT_DIR \
      --log-level=info \
      --local state.highstate
-  chmod -R 777 .tmp 
+  chmod -R 777 .tmp
 fi
 EOF
