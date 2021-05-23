184f9731a450cc84fcd37896e93dcd00dbbd4b25
verhulstm <michael@terminallabs.com>
2021-05-23 16:19:31 -0500

    cleaning

---
+++ configuration/namespaces/types/fuzzball/steps.sh
@@ -0,0 +1,5 @@
+bash .tmp/bash-environment-manager-master/configuration/namespaces/emitters/python/emit_activate.sh
+bash .tmp/bash-environment-manager-master/configuration/namespaces/runners/python/init.sh
+bash .tmp/bash-environment-manager-master/configuration/namespaces/runners/python/show_versions.sh
+bash .tmp/bash-environment-manager-master/configuration/namespaces/runners/python/run_tests.sh
+bash .tmp/bash-environment-manager-master/configuration/namespaces/runners/python/run.sh
